d87acd8f8cda2f84543622b5a8e1b7f9a415faf1
Amit Mittal <amit.mittal@iongroup.com>
2023-12-30 17:47:25 +0530

    using synthetic voice to announce numbers and changing theme to boss baby

--- src/App.tsx
+++ src/App.tsx
@@ -2,38 +2,47 @@ import { useState } from 'react';
 import backgroundImg from './background.png'
 import './App.css';
 import { Announcer } from './Announcer';
+import number_calling from "./number_calling_lines.json";
 
 const maxNum = 90;
-const perRowCount = 10;
-let enabled = true;
+const perRowCount = 9;
+let voice: SpeechSynthesisVoice;
 
 let announcer = new Announcer(maxNum);
 
 function App() {
   const [lastNumAnnounced, setLastNumAnnounced] = useState<number>();
+  const [enabled, setEnabled] = useState<boolean>(true);
+  let [voicesLoaded, setVoicesLoaded] = useState<boolean>(false);
+
+  speechSynthesis.onvoiceschanged = ()=>{
+    voice = speechSynthesis.getVoices().filter(v => v.name === "Microsoft Swara Online (Natural) - Hindi (India)")[0];
+    setVoicesLoaded(true);
+  }
   
   return (
     <div className='App' style={{ backgroundImage: `url(${backgroundImg})` }}>
       <div className="App-main">
         <header className='App-header'>
-          <div className='App-title'>Welcome to !!!Masha & Bear Housie!!!</div>
+          {/* <div className='App-title'>Welcome to !!!Baby Boss Housie!!!</div> */}
           <div className='App-actions'>
-            <button style={{gridColumn: 2}} onClick={()=>{
-              if(!enabled) return;
+            <button disabled={!enabled || !voicesLoaded} style={{gridColumn: 2}} onClick={()=>{
               const announced = announcer.next();
               if(announced > 0) {
-                setLastNumAnnounced(announced)
-                playAnnouncedNumber();
+                setEnabled(false);
+                setLastNumAnnounced(announced);
+                playAnnouncedNumber().then(()=>setEnabled(true));
               };
             }
             }>Next
             </button>
-            <button style={{gridColumn: 4}} onClick={()=>{
-              playAnnouncedNumber();
+            <button disabled={!enabled || !voicesLoaded} style={{gridColumn: 4}} onClick={()=>{
+              setEnabled(false);
+              playAnnouncedNumber().then(()=>setEnabled(true));
             }
             }>Play again
             </button>
-            <button style={{gridColumn: 6}} onClick={()=>{
+            <button disabled={!enabled || !voicesLoaded} style={{gridColumn: 6}} onClick={()=>{
               announcer = new Announcer(maxNum);
               setLastNumAnnounced(undefined);
             }
@@ -51,7 +60,7 @@ function App() {
                 return <tr key={i}>
                   {
                   [...Array(perRowCount)].filter((_, n)=> i + n <= maxNum).map((_, n)=>{
-                    return TambolaNum(i + n)
+                    return TambolaNum(i + n, enabled)
                   })
                   }
                 </tr>
@@ -63,32 +72,44 @@ function App() {
         </header>
       </div>
       <footer className='App-footer'>
-        <p style={{gridColumn: 2, gridRow: 3}}>Numbers announced {announcer.allAnnounced.length}/{maxNum}</p>
-        <p style={{gridColumn: 4, gridRow: 3}}>Last number announced: {lastNumAnnounced}</p>
-        <p style={{gridColumnStart: 2, gridColumnEnd: 5, gridRow: 4, color: 'white'}}>For Sammy-Shoi, with <span style={{color: "red", 'fontSize':25}}>&#10084;</span> from Fufu</p>
+        <p style={{gridColumn: 3, gridRow: 3}}>Announced: {announcer.allAnnounced.length}/{maxNum}</p>
+        <p style={{gridColumn: 5, gridRow: 3}}>Last number: {lastNumAnnounced}</p>
+        <p style={{gridColumnStart: 3, gridColumnEnd: 6, gridRow: 5, fontSize: 17, color: 'white'}}>For Anmay, with <span style={{color: "red", 'fontSize':25}}>&#10084;</span> from Papa</p>
       </footer>
     </div>
   );
 }
 
 function playAnnouncedNumber() {
-  if(!enabled)return;
-  enabled = false;
-  const sound = `${process.env.PUBLIC_URL}/sound/${announcer.lastAnnounced()}.opus`;
-  new Audio(sound).play().then
-  (()=>{
-    return new Promise((r)=>{
-      setTimeout(() => {
-        r(undefined);
-      }, 7000);
-    })
-  }).then(()=>{
-    enabled = true;
+  return new Promise<void>((resolve)=>{
+    if(!announcer.lastAnnounced()) resolve();
+  
+    const utterance = new SpeechSynthesisUtterance();
+    utterance.onend = ()=>{
+      console.log("end");
+      resolve();
+    }
+    utterance.voice = voice;
+    utterance.lang = voice.lang;
+    const num = number_calling as any;
+    utterance.text = num[announcer.lastAnnounced().toString()];
+    speechSynthesis.speak(utterance);
+    // const sound = `${process.env.PUBLIC_URL}/sound/${announcer.lastAnnounced()}.opus`;
+    // new Audio(sound).play().then
+    // (()=>{
+    //   return new Promise((r)=>{
+    //     setTimeout(() => {
+    //       r(undefined);
+    //     }, 7000);
+    //   })
+    // }).then(()=>{
+    //   resolve();
+    // })
   })
 }
 
-function TambolaNum(num : number) {
-  return <td className={announcer.hasBeenAnnounced(num)? "Announced" : ""}>{num}</td>
+function TambolaNum(num : number, enabled: boolean) {
+  return <td key={num} className={announcer.hasBeenAnnounced(num)? (num === announcer.lastAnnounced() && !enabled ? "last-Announced": "Announced") : ""}>{num}</td>
 }
 
 export default App;
